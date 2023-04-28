9ad496f22daaf48b4c8dde21ee41119f22d950d4
CsChinmay <110757877+CsChinmay@users.noreply.github.com>
2023-04-28 16:32:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Filestore:A2ZmoviesCS@fil
 DB_NAME = os.environ.get("DATABASE_NAME", "A2Zstore")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001865179980"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
